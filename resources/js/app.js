50dfed5b0319984786eab6d3f41b2ccecc198964
csetowhid <towhidhasang1@gmail.com>
2020-12-13 10:30:50 +0600

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+require('./bootstrap');
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+
+const app = new Vue({
+    el: '#app',
+    router
+});
